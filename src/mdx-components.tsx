237fe7d25c01e8695340b150895f21976da3a943
Saksit Sirisakda <saksit.sirisakda@cr3.group>
2025-07-17 16:55:22 +0700

    add lint

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -4,7 +4,12 @@ import Image, { ImageProps } from 'next/image';
 export function useMDXComponents(components: MDXComponents): MDXComponents {
     return {
         img: (props) => (
-            <Image width={1200} height={600} {...(props as ImageProps)} />
+            <Image
+                {...(props as ImageProps)}
+                width={1200}
+                height={600}
+                alt={props.alt}
+            />
         ),
         ...components,
     };

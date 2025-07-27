b428d7a31ed8afe3519a504635ed78a214dc0d25
FOLK <folkadpocket99@gmail.com>
2025-07-27 16:22:36 +0700

    update animation duration

--- src/components/AnimatedWrapper.tsx
+++ src/components/AnimatedWrapper.tsx
@@ -18,7 +18,7 @@ export default function AnimatedWrapper({
                 initial={{ opacity: 0, y: 50 }}
                 animate={{ opacity: 1, y: 0 }}
                 exit={{ opacity: 0, y: -50 }}
-                transition={{ duration: 0.5 }}
+                transition={{ duration: 0.3 }}
             >
                 {children}
             </motion.div>
